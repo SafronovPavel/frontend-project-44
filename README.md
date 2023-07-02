786f2d32a6db77d12e8be36d1be236249fc52cb8
PAVEL SAFRONOV <908916@gmail.com>
2023-07-02 16:56:18 +0700

    completed fifth step

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/SafronovPavel/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/SafronovPavel/frontend-project-44/actions)
 <a href="https://codeclimate.com/github/SafronovPavel/frontend-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/8095f8e9c0827dd847f4/maintainability" /></a>
+<a href="https://asciinema.org/a/594302" target="_blank"><img src="https://asciinema.org/a/594302.svg" /></a>
---
+++ src/games/brain-even.js
@@ -0,0 +1,48 @@
+import readlineSync from 'readline-sync';
+
+// получение рандомного числа
+const getRandomInt = () => {
+  return Math.floor(Math.random() * 100);
+};
+
+// приветствие
+const welcome = () => {
+  
+
+};
+
+
+const brainEven = () => {
+  console.log('Welcome to the Brain Games!');
+  const nameUser = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${nameUser}!`);
+
+  console.log('Answer "yes" if the number is even, otherwise answer "no".')
+  
+  for (let i = 0; i < 3; i += 1) {
+    let randomNumber = getRandomInt();
+
+    console.log(`Question: ${randomNumber}`);
+    const evenNumber = (number) => number % 2 === 0;
+    
+    const answerComputer = evenNumber(randomNumber) ? 'yes' : 'no';
+    const answerUser = readlineSync.question('Your answer: ');
+    
+    if (answerComputer === answerUser && i === 2) {
+      console.log(`Correct!\nCongratulations, ${nameUser}!`);
+    } else if (answerComputer === answerUser && i < 2) {
+      console.log('Correct!');
+    } else if (answerComputer !== answerUser) {
+      console.log(`'${answerUser}' is wrong answer ;(. Correct answer was '${answerComputer}'.`);
+      console.log(`Let's try again, ${nameUser}!`);
+
+
+      break;
+    }
+  }
+  
+
+};
+
+
+export default brainEven;
