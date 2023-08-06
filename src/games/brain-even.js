42157cfddd04c7957bfc3322907c6d536f671f38
PAVEL SAFRONOV <908916@gmail.com>
2023-08-06 18:34:04 +0700

    rename isEvenNumber

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -3,12 +3,12 @@ import getRandom from '../utils.js';
 
 const taskForUser = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const evenNumber = (number) => number % 2 === 0;
+const isEvenNumber = (number) => number % 2 === 0;
 
 const circle = () => {
   const randomNumber = getRandom(1, 100);
   const question = `${randomNumber}`;
-  const correctAnswer = evenNumber(randomNumber) ? 'yes' : 'no';
+  const correctAnswer = isEvenNumber(randomNumber) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
