9eb249659b6fcc6aff47204256a760fa087d5518
PAVEL SAFRONOV <908916@gmail.com>
2023-08-06 21:17:53 +0700

    made calculate apart

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -2,18 +2,27 @@ import runGame from '../index.js';
 import getRandom from '../utils.js';
 
 const taskForUser = 'What is the result of the expression?';
+const operators = ['+', '-', '*'];
+
+const calculate = (randomCharacter, randomNumber1, randomNumber2) => {
+  switch (randomCharacter) {
+    case '+':
+      return randomNumber1 + randomNumber2;
+    case '-':
+      return randomNumber1 - randomNumber2;
+    case '*':
+      return randomNumber1 * randomNumber2;
+    default:
+      console.log(`Unknown character: ${randomCharacter}!`);
+  }
+};
 
 const getQuestion = () => {
   const randomNumber1 = getRandom();
   const randomNumber2 = getRandom();
-  const sum = randomNumber1 + randomNumber2;
-  const subtraction = randomNumber1 - randomNumber2;
-  const multiplication = randomNumber1 * randomNumber2;
-  const operations = [sum, subtraction, multiplication];
-  const operators = ['+', '-', '*'];
-  const randomCharacter = getRandom(0, operators.length - 1);
-  const question = `${randomNumber1} ${operators[randomCharacter]} ${randomNumber2}`;
-  const correctAnswer = String(operations[randomCharacter]);
+  const randomCharacter = operators[getRandom(0, operators.length - 1)];
+  const question = `${randomNumber1} ${randomCharacter} ${randomNumber2}`;
+  const correctAnswer = String(calculate(randomCharacter, randomNumber1, randomNumber2));
   return [question, correctAnswer];
 };
 
