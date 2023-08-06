3e03a0bdd5a5319b2d42cd82d8d740ad940f9b62
PAVEL SAFRONOV <908916@gmail.com>
2023-08-06 19:06:10 +0700

    rename isTestPrime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -3,7 +3,7 @@ import getRandom from '../utils.js';
 
 const taskForUser = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const testPrime = (n) => {
+const isTestPrime = (n) => {
   if (n === 1) {
     return false;
   } if (n === 2) {
@@ -20,7 +20,7 @@ const testPrime = (n) => {
 const getQuestion = () => {
   const randomNumber = getRandom(1, 100);
   const question = `${randomNumber}`;
-  const correctAnswer = testPrime(randomNumber) ? 'yes' : 'no';
+  const correctAnswer = isTestPrime(randomNumber) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
 
