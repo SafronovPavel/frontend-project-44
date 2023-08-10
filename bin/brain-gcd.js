893f1d1cb315752f0fff469064c95b25ec352f14
PAVEL SAFRONOV <908916@gmail.com>
2023-08-10 17:56:35 +0700

    rewrite Gcd > GCD

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 import runBrainGcd from '../src/games/brain-gcd.js';
 
-runBrainGcd();
+runBrainGCD();
--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -3,10 +3,10 @@ import getRandomNumber from '../utils.js';
 
 const taskForUser = 'Find the greatest common divisor of given numbers.';
 
-const findGcd = (number1, number2) => {
+const findGCD = (number1, number2) => {
   if (number2 !== 0) {
     const remainder = number1 % number2;
-    return findGcd(number2, remainder);
+    return findGCD(number2, remainder);
   }
   return number1;
 };
@@ -15,7 +15,7 @@ const getRound = () => {
   const Number1 = getRandomNumber();
   const Number2 = getRandomNumber();
   const question = `${Number1} ${Number2}`;
-  const correctAnswer = String(findGcd(Number1, Number2));
+  const correctAnswer = String(findGCD(Number1, Number2));
   return [question, correctAnswer];
 };
 
